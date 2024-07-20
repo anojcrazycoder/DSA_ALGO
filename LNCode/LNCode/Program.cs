e0df2ca70f0460f0c5fc17ffeebf33a72b933c20
Anoj Kumar Shrestha <anojbestchoices@gmail.com>
2024-07-20 11:33:20 -0500

    Completed valid Parenthesis using stack #leetcode 20

--- LNCode/LNCode/Program.cs
+++ LNCode/LNCode/Program.cs
@@ -1,15 +1,47 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using LNCode.Array;
-
-Console.WriteLine("Hello, World!");
-
-List<string> list = new List<string>();
-list.Add("Anoj#");
-list.Add("##Akshada");
-list.Add("*Raju**");
+using LNCode.Array.Easy;
+using LNCode.Array.Medium;
+using LNCode.Array.SlidingWindow;
+using LNCode.StackDemo;
 
 
+Console.WriteLine(MyStack.IsValid("]]"));
 
-string a = EncodeDecode.Encode(list);
 
-List<String> list2 = EncodeDecode.Decode(a);
+
+
+
+
+
+//Sliding window practice
+
+//double maxAverage = SlidingWindowProb.MaximumAverageSubArray(new int[] { 4, 0, 4, 3, 3 }, 5);
+//Console.WriteLine(maxAverage);
+
+//Console.WriteLine(Anagram.IsAnagram("jar", "jam"));
+
+//int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+//int k = 3;
+//Console.WriteLine(MaxSumSubarray.MaxSumSubarray1(arr, k));  // Output: 27 (8 + 9 + 10)
+
+
+//int[] a= TopKElementSolution.topKFrequent(new int[] { 1, 1, 1, 1, 2, 2, 3, 3, 4 },2);
+
+
+//int[] a = { 1, 2, 4, 6 };
+//ProductExceptSelf.ProductExceptSelfFun(a);
+
+
+//Console.WriteLine("Hello, World!");
+
+//List<string> list = new List<string>();
+//list.Add("Anoj#");
+//list.Add("##Akshada");
+//list.Add("*Raju**");
+
+
+
+//string a = EncodeDecode.Encode(list);
+
+//List<String> list2 = EncodeDecode.Decode(a);
---
+++ LNCode/LNCode/StackDemo/MyStack.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LNCode.StackDemo
+{
+    internal class MyStack
+    {
+        public static bool IsValid(string s)
+        {
+            Stack<char> stack = new Stack<char>();
+
+            foreach (char c in s)
+            {
+                if (c == '[')
+                {
+                    stack.Push(']');
+
+                }
+                else if (c == '{')
+                {
+                    stack.Push('}');
+
+                }
+                else if (c == '(')
+                {
+                    stack.Push(')');
+
+                }
+
+                else if (stack.Count ==0 || c != stack.Pop()) {
+                    return false;
+                }
+
+            }
+            if (s.Length == 1) return false;
+            return stack.Count==0;
+
+        }
+    }
+}
